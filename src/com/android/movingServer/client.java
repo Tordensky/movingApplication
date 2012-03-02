aa7d427cae1f73d527e3fd6d3dfbf37c78e564cf
Simon Nistad <simon.nistad@hotmail.com>
2012-03-02 11:05:13 +0100

    Added a server class

---
+++ src/com/android/movingServer/client.java
@@ -0,0 +1,15 @@
+package com.android.movingServer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class client extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
